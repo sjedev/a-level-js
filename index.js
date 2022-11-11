14234b102a75b397213d3e0cb6db4b382de77b76
S Jensen <111765576+sjedev@users.noreply.github.com>
2022-11-11 11:42:02 +0000

    Frequency

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 var prompt = require("prompt-sync")();
 
 var phrase = "the thing about the word the is that the is one of the most commonly used words"
-var noDuplicates = []
+var frequency = []
 
 var phraseUpper = phrase.toUpperCase()
-var phraseSplit = phraseUpper.split(" ")
+var phraseSplit = phraseUpper.split("")
 
-for(let i in phraseSplit){
-    if(noDuplicates.indexOf(phraseSplit[i]) == -1){
-        noDuplicates.push(phraseSplit[i])
-    }
-}
+frequency.push(phraseSplit[0])
+frequency[0].push("1")
 
-console.log(noDuplicates)
+console.log(frequency)

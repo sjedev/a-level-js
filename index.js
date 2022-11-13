18ddedc1d6dc035bdd90d4f64d1b24ca343f4447
S Jensen <111765576+sjedev@users.noreply.github.com>
2022-11-13 19:52:01 +0000

    Letter frequency analysis

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
 var prompt = require("prompt-sync")();
-
-var phrase = "the thing about the word the is that the is one of the most commonly used words"
-var frequency = []
-
-var phraseUpper = phrase.toUpperCase()
-var phraseSplit = phraseUpper.split("")
-
-frequency.push(phraseSplit[0])
-frequency[0].push("1")
-
-console.log(frequency)
